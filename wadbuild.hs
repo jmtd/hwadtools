3a5abf87de166b17e0858f2956ee2b1aa0ee0505
Jonathan Dowland <jon@dow.land>
2017-08-30 09:44:02 +0100

    wadbuild: split file parser into header,body
    solves the issue of mandatory trailing newline after the [IP]WAD
    marker.
    
    Juggle the tests a bit accordingly: I briefly had 100% coverage!
    
    Recategorise test_14 as a must-fail: we will shortly disallow a
    space char (Amongst others) as valid in a label.

--- wadbuild.hs
+++ wadbuild.hs
@@ -19,15 +19,19 @@ data WadInfoCommand = WadInfoLabel String | SomethingElse
     | IWAD | PWAD
     deriving (Show,Eq)
 
--- it would be nice to extend this to support parsing the wadMagic before any other commands 
--- with comments/whitespace interspersed freely around them
 wadInfoFile = do
-    skipMany ((wadInfoComment <|> emptyLine) >> eol)
-    magic <- wadMagic
-    eol
-    result <- wadInfoLine `sepEndBy` eol
+    magic  <- wadInfoHeader
+    result <- option [] wadInfoBody
     eof
     return (magic:(filter ((/=) SomethingElse) result))
+
+wadInfoHeader = do
+    skipMany ((wadInfoComment <|> emptyLine) >> eol)
+    wadMagic
+
+wadInfoBody = do
+    eol -- after the header
+    wadInfoLine `sepEndBy` eol
 
 eol = char '\n'
 
@@ -68,7 +72,8 @@ parsePatch x = parse wadInfoFile "" x
 
 -- good test data
 -- XXX: need to firm these up with comparisons to literal parse trees
-test_0 = (assertRight . parsePatch)  "IWAD\n"             -- simplest
+test_25 = (assertRight . parsePatch) "IWAD"               -- simple
+test_0 = (assertRight . parsePatch)  "IWAD\n"             -- still simple
 test_1 = (assertRight . parsePatch)  "PWAD\n"
 test_2 = (assertRight . parsePatch)  "#comment\nIWAD\n"   -- pre-comment
 test_3 = (assertRight . parsePatch)  "PWAD\n#comment"     -- post-comment
@@ -79,22 +84,21 @@ test_9 = (assertRight . parsePatch)  "PWAD\nlabel MAP01"  -- valid label (<8 len
 test_10 = (assertRight . parsePatch) "PWAD\nlabel MAP01\nlabel MAP01"  -- valid labels (duplicated)
 test_11 = (assertRight . parsePatch) "PWAD\nlabel 01234567" -- valid label (==8 length)
 test_12 = (assertRight . parsePatch) "PWAD\nlabel 01234567 " -- valid label (whitespace suffix)
-test_13 = (assertRight . parsePatch) "PWAD\nlabel 01234567#" -- valid label (empty comment suffix)
-test_14 = (assertRight . parsePatch) "PWAD\nlabel 01234567# comment" -- valid label (comment suffix)
 
 -- bad test data
 test_4 = (assertLeft . parsePatch)  "PWAD#comment"     -- suffixed comment not supported
 test_5 = (assertLeft . parsePatch)  "PWAD #comment"    -- suffixed whitespace (and comment)
-test_15 = (assertLeft . parsePatch) "IWAD\n# blah\nIWAD" -- too many magics -- failing
-test_16 = (assertLeft . parsePatch) "IWAD\nPWAD"         -- too many magics -- failing
+test_15 = (assertLeft . parsePatch) "IWAD\n# blah\nIWAD" -- too many magics
+test_16 = (assertLeft . parsePatch) "IWAD\nPWAD"         -- too many magics
 test_17 = (assertLeft . parsePatch) "JWAD"               -- invalid magic
-test_18 = (assertLeft . parsePatch) ""                   -- missing commands -- failing
-test_19 = (assertLeft . parsePatch) "\n" -- failing
-test_20 = (assertLeft . parsePatch) "# nothing here" -- failing
-test_21 = (assertLeft . parsePatch) "label foo"          -- missing magic -- failing
-test_22 = (assertLeft . parsePatch) "label foo\nIWAD"    -- magic not first -- failing
-test_23 = (assertLeft . parsePatch) "PWAD\nlabel 123456789" -- invalid label (>8 length) -- failing
+test_18 = (assertLeft . parsePatch) ""                   -- missing commands
+test_19 = (assertLeft . parsePatch) "\n"
+test_20 = (assertLeft . parsePatch) "# nothing here"
+test_21 = (assertLeft . parsePatch) "label foo"          -- missing magic
+test_22 = (assertLeft . parsePatch) "label foo\nIWAD"    -- magic not first
 test_24 = (assertLeft . parsePatch) "    IWAD"
-test_25 = (assertLeft . parsePatch) "IWAD" -- missing newline suffix
+test_14 = (assertLeft . parsePatch) "PWAD\nlabel 01234567# comment" -- comment suffixwes not supported
+
+-- XXX: write QP tests for label values
 
 main = htfMain htf_thisModulesTests
