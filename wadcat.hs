165146b777f866eae9278811b9ed76a572ee009a
Jonathan Dowland <jon@dow.land>
2018-02-14 17:24:41 +0000

    wadcat: concatenate wad files

---
+++ wadcat.hs
@@ -0,0 +1,53 @@
+{-
+    wadcat.hs: concatenate a series of WADs into one
+    Part of HWadTools
+    Copyright © Jonathan Dowland 2018
+
+    Distributed under the terms of the GNU GPL Version 3
+    See file LICENSE
+ -}
+
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as L
+import qualified Data.ByteString.Lazy.Char8 as LC
+import qualified Data.ByteString.Char8 as C
+import Data.Binary (encode, decode)
+import Data.Binary.Get
+import System.IO (stdin, IOMode(..), hSetBinaryMode, openFile, Handle)
+import System.Environment (getArgs)
+import Data.Algorithm.Diff
+import Data.Algorithm.DiffContext
+import Text.PrettyPrint
+import Data.Int (Int32)
+
+import Wad
+
+getHandles :: IO ([Handle], Handle)
+getHandles = do
+    args    <- getArgs
+    out     <- openFile (head $ reverse args) WriteMode
+    handles <- mapM (\x-> openFile x ReadMode) $ (reverse.tail.reverse) args
+
+    hSetBinaryMode out True
+    mapM_ (\x->hSetBinaryMode x True) handles
+
+    return (handles, out)
+
+main = do
+    (handles, out) <- getHandles
+    inputs         <- mapM L.hGetContents handles -- :: [L.ByteString]
+
+    let dirs       = map wadDirEnts inputs -- :: [[DirEnt]]
+        headers    = map decode inputs :: [WadHeader]
+        outNuments = fromIntegral $ sum $ map length dirs
+        outDiroffs = wadHeaderSize + (fromIntegral $ sum $ map (\wh->((diroffs wh) - (fromIntegral wadHeaderSize))) headers)
+        outHeader  = WadHeader (LC.pack "PWAD") outNuments (fromIntegral outDiroffs)
+        in do
+            L.hPut out (encode outHeader)
+            mapM_ (writeLumps out) $ zip headers inputs
+            -- XXX directories need accumulated offset rewriting
+            mapM_ (L.hPut out) $ concat $ map (map encode) dirs
+
+writeLumps :: Handle -> (WadHeader, L.ByteString) -> IO ()
+writeLumps out (header,input) = do
+    L.hPut out $ L.drop (fromIntegral wadHeaderSize) $ L.take (fromIntegral (diroffs header)) input
